6d49f0047f36c9e8f41f89dc145aa024e4721540
nsadassi <47839337+nsadassi@users.noreply.github.com>
2020-11-11 16:18:00 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Chart from './Chart';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+import Home from './Pages/Home';
+import BarChart from './Pages/BarChart';
+import LineChart from './Pages/LineChart';
 
 function App() {
   return (
     <div className="App">
-      <Chart />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/BarChart' component={BarChart}/>
+            <Route path='/LineChart' component={LineChart}/>
+          </Switch>
+        </Router>
     </div>
   );
 }
